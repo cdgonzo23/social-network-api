06b1b41be3f1c93ffcbd2461251da9cef39135ae
Christian Gonzales <124095890+cdgonzo23@users.noreply.github.com>
2023-08-22 12:22:04 -0700

    created models

---
+++ models/User.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: [true, 'Username is require.'],
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: [true, 'User email is require.'],
+            trim: true,
+            validate: {
+                validator: function(v) {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+                },
+                message: props => `${props.value} is not a valid email.`
+            }
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length
+    });
+
+const User = model('user', userSchema);
+
+
+module.exports = User;
